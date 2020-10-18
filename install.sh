9f7ec42c5e6f712fe98f5f58ef0add6045fb0f0b
junnys6018 <junkanglim1@gmail.com>
2020-10-18 09:35:27 +0800

    fixed install script for git bash in windows

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 ########## Variables
 
-dir=~/.dotfiles                    # dotfiles directory
+dir=$HOME/.dotfiles                    # dotfiles directory
 files=".bashrc .bash_profile .gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir
+cd "$dir"
 echo "...done"
 
 # create symlinks 
 for file in $files; do
     echo "Creating symlink to $file in home directory."
-	rm ~/$file
-    ln -s $dir/$file ~/$file
+    if [ -f "$HOME/$file" ]; then
+        echo "hi"
+	    rm "$HOME/$file"
+    fi
+    ln -s "$dir/$file" "$HOME/$file"
 done
