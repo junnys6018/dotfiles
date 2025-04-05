b94c822d9a8afe0939386f42c910534111698b06
junnys6018 <48525532+junnys6018@users.noreply.github.com>
2025-04-05 10:34:31 +0800

    basic nvim config

---
+++ nvim/lua/options.lua
@@ -0,0 +1,24 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Decrease update time
+-- See `:help updatetime`
+vim.opt.updatetime = 250
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
