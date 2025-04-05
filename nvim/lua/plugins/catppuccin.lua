d1e92bab714978f96140bde4216d43326de93746
junnys6018 <48525532+junnys6018@users.noreply.github.com>
2025-04-05 17:50:21 +0800

    fix catppuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,14 @@
 return {
     'catppuccin/nvim',
+    lazy = false,
     name = 'catppuccin',
-    priority = 1000
+    priority = 1000,
+    config = function()
+        require('catppuccin').setup({
+            flavour = 'mocha',
+            -- transparent_background = true,
+            show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+        })
+        vim.cmd.colorscheme('catppuccin')
+    end
 }
